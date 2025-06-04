16daece20622b67023d9c8667fa2051bdf3570b8
Lucas Monteiro <lucas.mribeiro@aiqfome.com>
2025-06-04 08:51:45 -0300

    init: 🎉

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,12 @@
+import { colors } from '@lumr-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+})
+
+export function App() {
+  return <Button>Hello World</Button>
+}
